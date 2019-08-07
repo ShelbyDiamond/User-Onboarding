f2b9a0c764f77d3271fef23ed27d96dad86bb477
Shelby Diamond <Shelbydiamond518@gmail.com>
2019-08-07 15:26:33 -0700

    added new file Form and updated code.
    still need to get submit button working

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,7 +1,68 @@
-import React from "react";
+import React, { useState, useEffect } from "react"
+import { Form, Field, withFormik } from "formik"
+import * as Yup from "yup"
+import axios from "axios"
 
-const Form = () => {
-  return <div />;
-};
+const OnboardingForm = ({ touched, errors, status, values, handleSubmit }) => {
+  const [user, setUser] = useState([])
 
-export default Form;
+  useEffect(() => {
+    if (status) {
+      setUser([...user, status])
+    }
+  }, [status])
+
+  return (
+    <div className="App">
+      <h1>New User Sign Up</h1>
+      <Form>
+        <Field type="text" name="name" placeholder="Name" />
+        {touched.name && errors.name && <p className="error"> {errors.name}</p>}
+        <Field type="text" name="password" placeholder="Password" />
+        {touched.password && errors.password && (
+          <p className="error">{errors.password}</p>
+        )}
+        <Field type="email" name="email" placeholder="Email" />
+        {touched.email && errors.email && (
+          <p className="error">{errors.email}</p>
+        )}
+        <label className="checkbox">
+          I have Read and Accept the Terms of Service
+          <Field
+            type="checkbox"
+            name="serviceTerms"
+            checked={values.serviceTerms}
+          />
+        </label>
+        <button type="submit">Submit</button>
+      </Form>
+    </div>
+  )
+}
+
+const FormikOnboardingForm = withFormik({
+  mapPropsToValues({ name, password, email }) {
+    return {
+      name: name || "",
+      password: password || "",
+      email: email || ""
+    }
+  },
+
+  validateSchema: Yup.object().shape({
+    password: Yup.string().required("ayye")
+  }),
+
+  handleSubmit(values, { setStatus }) {
+    axios
+      .post("https://reqres.in/api/users/", values)
+      .then(res => {
+        console.log("post api response", res)
+        setStatus(res.data)
+        console.log("current user", values)
+      })
+      .catch(err => console.log(err.response))
+  }
+})(OnboardingForm)
+
+export default FormikOnboardingForm
