626b5ca84f73753affcea2e9bea1118d8ab7c41e
Shelby Diamond <Shelbydiamond518@gmail.com>
2019-08-07 18:07:13 -0700

    met stretch goals by styling my page

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -2,6 +2,38 @@ import React, { useState, useEffect } from "react"
 import { Form, Field, withFormik } from "formik"
 import * as Yup from "yup"
 import axios from "axios"
+import styled from "styled-components"
+
+const Welcome = styled.h1`
+  font-size: 4rem;
+  color: purple;
+`
+const Name = styled.label`
+  color: purple;
+  display: inline-flex;
+  justify-content: center;
+  margin: 5px;
+`
+
+const Button = styled.button`
+  border-radius: 5px;
+  padding: 5px;
+  padding-left: 15px;
+  padding-right: 15px;
+  margin: 5px;
+  margin-left: 75px;
+  color: white;
+  background-color: blue;
+  display: flex;
+  justify-content: center;
+`
+
+const CheckBox = styled.label`
+  display: flex;
+  color: green;
+  font-size: 1.2rem;
+  margin: 25px;
+`
 
 const OnboardingForm = ({ touched, errors, status, values, handleSubmit }) => {
   const [user, setUser] = useState([])
@@ -14,27 +46,42 @@ const OnboardingForm = ({ touched, errors, status, values, handleSubmit }) => {
 
   return (
     <div className="App">
-      <h1>New User Sign Up</h1>
+      <Welcome>Welcome to my Kingdom</Welcome>
       <Form>
-        <Field type="text" name="name" placeholder="Name" />
-        {touched.name && errors.name && <p className="error"> {errors.name}</p>}
-        <Field type="text" name="password" placeholder="Password" />
-        {touched.password && errors.password && (
-          <p className="error">{errors.password}</p>
-        )}
-        <Field type="email" name="email" placeholder="Email" />
-        {touched.email && errors.email && (
-          <p className="error">{errors.email}</p>
-        )}
-        <label className="checkbox">
+        <Name>
+          Name:
+          <Field type="text" name="name" placeholder="Name" />
+          {touched.name && errors.name && (
+            <p className="error"> {errors.name}</p>
+          )}
+        </Name>
+
+        <Name>
+          Password:
+          <Field type="text" name="password" placeholder="Password" />
+          {touched.password && errors.password && (
+            <p className="error">{errors.password}</p>
+          )}
+        </Name>
+
+        <Name>
+          Email:
+          <Field type="email" name="email" placeholder="Email" />
+          {touched.email && errors.email && (
+            <p className="error">{errors.email}</p>
+          )}
+        </Name>
+
+        <CheckBox className="checkbox">
           I have Read and Accept the Terms of Service
           <Field
             type="checkbox"
             name="serviceTerms"
             checked={values.serviceTerms}
           />
-        </label>
-        <button type="submit">Submit</button>
+        </CheckBox>
+
+        <Button type="submit">Submit</Button>
 
         {user.map(person => (
           <p key={person.id}>{person.name}</p>
